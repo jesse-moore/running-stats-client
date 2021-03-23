8397e17112b16d9469a2e5fb2f986c7e569568c8
Jesse <moore.jesse@gmail.com>
2021-03-23 18:17:14 -0500

    removed console.log

--- src/pages/List.js
+++ src/pages/List.js
@@ -8,8 +8,6 @@ import Loading from "../components/Loading";
 
 const List = ({ activeMonth, activeYear, unit }) => {
   const [fetchActivities, { data, loading, error }] = useLazyQuery(ACTIVITIES);
-
-  if (error) console.log(error);
   useEffect(() => {
     if (activeMonth === null || activeYear === null) return;
     fetchActivities({
@@ -26,49 +24,54 @@ const List = ({ activeMonth, activeYear, unit }) => {
 
   return (
     <div className="relative">
-      {loading && <Loading />}
-      <div className="mx-auto w-max">
-        {activities.map((activity) => {
-          const {
-            name,
-            distance,
-            date,
-            elevationGain,
-            averageSpeed,
-            movingTime,
-            map,
-            location,
-            weather,
-          } = activity;
-          return (
-            <div className="my-4 px-4 py-4 bg-white shadow rounded flex flex-row justify-between w-full">
-              <div className="flex flex-col mr-4">
-                <div className="pr-2 mb-2 flex flex-col items-baseline">
-                  <div className="text-lg">{name}</div>
-                  {location && <Location location={location} />}
-                  <div className="text-sm text-gray-500">{date}</div>
-                </div>
-                <div className="flex flex-row text-lg mb-2">
-                  <div className="pr-2">{`${distance.value}${distance.unit}`}</div>
-                  <div className="px-2">{`${averageSpeed.value}${averageSpeed.unit}`}</div>
-                  <div className="px-2">{movingTime.value}</div>
-                  <div className="px-2">
-                    {elevationGain.value}
-                    {elevationGain.unit}
-                  </div>
-                </div>
-                {weather && <WeatherData weather={weather} />}
-              </div>
-              <div>
-                <img
-                  src={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/path-2+c40000-1(${encodeURIComponent(
-                    map.summary_polyline
-                  )})/auto/200x200?access_token=${mapBoxKey}&padding=30`}
-                />
-              </div>
-            </div>
-          );
-        })}
+      {(!data || loading) && <Loading />}
+      <div className="mx-auto max-w-max">
+        {activities.map((activity) => (
+          <Activity activity={activity} key={activity.dateUnix} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Activity = ({ activity }) => {
+  const {
+    name,
+    distance,
+    date,
+    elevationGain,
+    averageSpeed,
+    movingTime,
+    map,
+    location,
+    weather,
+  } = activity;
+  return (
+    <div className="my-4 px-4 py-4 bg-white shadow rounded flex sm:flex-row flex-col sm:justify-between">
+      <div className="flex flex-col mr-4 flex-shrink-0">
+        <div className="pr-2 mb-2 flex flex-col items-baseline">
+          <div className="text-lg">{name}</div>
+          {location && <Location location={location} />}
+          <div className="text-sm text-gray-500">{date}</div>
+        </div>
+        <div className="flex flex-row text-base sm:text-lg mb-2 flex-wrap">
+          <div className="pr-2">{`${distance.value}${distance.unit}`}</div>
+          <div className="px-2">{`${averageSpeed.value}${averageSpeed.unit}`}</div>
+          <div className="px-2">{movingTime.value}</div>
+          <div className="px-2">
+            {elevationGain.value}
+            {elevationGain.unit}
+          </div>
+        </div>
+        {weather && <WeatherData weather={weather} />}
+      </div>
+      <div>
+        <img
+          className="sm:mx-0 mx-auto"
+          src={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/path-2+c40000-1(${encodeURIComponent(
+            map.summary_polyline
+          )})/auto/200x200?access_token=${mapBoxKey}&padding=30`}
+        />
       </div>
     </div>
   );
